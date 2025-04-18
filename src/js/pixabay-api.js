9d37d7a661c96a9afa6bb5296b4ff19d68ffd114
Andrii S <semtrololo@gmail.com>
2025-04-18 17:15:03 +0200

    add filters

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -6,15 +6,23 @@ import 'izitoast/dist/css/iziToast.min.css';
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '49684635-7a6aa23e2b6de301ea62c53b0';
 
-export async function getImagesByQuery(searchText, page = 1) {
+export async function getImagesByQuery({
+  q,
+  page = 1,
+  colors,
+  category,
+  per_page,
+}) {
   const response = await axios.get(`${BASE_URL}`, {
     params: {
       key: API_KEY,
-      q: searchText,
+      q,
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: 'true',
-      per_page: 15,
+      per_page,
+      colors,
+      category,
       page,
     },
   });
